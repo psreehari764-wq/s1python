5c1989e0e417b8e070b423e7d9c1b761670fab4d
00midhun00 <midhuntjoseph03@gmail.com>
2025-10-30 15:57:17 +0530

    last commit

---
+++ C3/graphics/circle.py
@@ -0,0 +1,5 @@
+import math
+def area(radius):
+    return math.pi * radius *radius
+def circumference(radius):
+    return 2* math.pi * radius
---
+++ C3/main.py
@@ -0,0 +1,10 @@
+import graphics.rectangle
+import graphics.circle
+import graphics.graphics_3d.cuboid
+import graphics.graphics_3d.sphere
+
+print("rect area",graphics.rectangle.area(3))
+print("cub peri",graphics.graphics_3d.cuboid.volume(5,7,9))
+print("sphere area",graphics.graphics_3d.sphere.volume(6))
+print("circle area",graphics.circle.area(5))
+print("cub volume",graphics.graphics_3d.cuboid.surface_are(2,5,9))
